6e584441e10d644948d6e0ea7af217112a1d161a
wj <jian.lao1314@gmail.com>
2025-06-10 17:02:14 +0800

    robots接收所有爬虫

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,31 +1,16 @@
 import type { APIRoute } from 'astro';
 
 const getRobotsTxt = (sitemapURL: URL) => `User-agent: *
-Disallow: /MineWeb/
-Disallow: /assets/
-Disallow: /*?*
-Disallow: /api/
-Disallow: /admin/
-
-# 控制爬虫访问频率
-Crawl-delay: 10
-
-# 允许搜索引擎访问
-User-agent: Baiduspider
 Allow: /
-Crawl-delay: 5
 
-User-agent: Googlebot
-Allow: /
-Crawl-delay: 5
-
-User-agent: Bingbot
-Allow: /
-Crawl-delay: 5
 
 Sitemap: ${sitemapURL.href}`;
 
 export const GET: APIRoute = ({ site }) => {
   const sitemapURL = new URL('sitemap-index.xml', site);
-  return new Response(getRobotsTxt(sitemapURL));
+  return new Response(getRobotsTxt(sitemapURL), {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8'
+    }
+  });
 };
